ed2ab7d0d071a5411fe8bdec4c5fb7547f1afa7e
Sahil Atahar <sahilatahar@gmail.com>
2025-06-09 01:20:54 +0530

    fix: recording issues in chrome

--- src/context/MediaContext.tsx
+++ src/context/MediaContext.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+import { MediaContextProps } from "@/types";
 import {
 	ReactElement,
 	ReactNode,
@@ -8,87 +9,107 @@ import {
 	useEffect,
 	useRef,
 	useState,
-} from "react"
-import { toast } from "react-hot-toast"
-import { MediaContextProps } from "@/types"
+} from "react";
+import { toast } from "react-hot-toast";
 
-const MediaContext = createContext<MediaContextProps | null>(null)
+const MediaContext = createContext<MediaContextProps | null>(null);
 
 export const useMediaContext = (): MediaContextProps => {
-	const context = useContext(MediaContext)
+	const context = useContext(MediaContext);
 	if (context == null) {
-		throw new Error("useMediaContext must be used within a MediaProvider")
+		throw new Error("useMediaContext must be used within a MediaProvider");
 	}
-	return context
+	return context;
+};
+
+function mergeAudioStreams(
+	screenStream: MediaStream,
+	micStream: MediaStream
+): MediaStream {
+	const context = new AudioContext();
+	const destination = context.createMediaStreamDestination();
+
+	if (screenStream.getAudioTracks().length > 0) {
+		const source1 = context.createMediaStreamSource(screenStream);
+		source1.connect(destination);
+	}
+
+	if (micStream.getAudioTracks().length > 0) {
+		const source2 = context.createMediaStreamSource(micStream);
+		source2.connect(destination);
+	}
+
+	// Combine video track(s) from screen with the merged audio tracks
+	const combinedStream = new MediaStream([
+		...screenStream.getVideoTracks(),
+		...destination.stream.getAudioTracks(),
+	]);
+
+	return combinedStream;
 }
 
 export const MediaProvider: React.FC<{ children: ReactNode }> = ({
 	children,
 }): ReactElement => {
-	const [recording, setRecording] = useState<boolean>(false)
-	const [stream, setStream] = useState<MediaStream | null>(null)
-	const [selectedOption, setSelectedOption] = useState<string>("")
+	const [recording, setRecording] = useState<boolean>(false);
+	const [stream, setStream] = useState<MediaStream | null>(null);
+	const [selectedOption, setSelectedOption] = useState<string>("");
 	const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
 		null
-	)
-	const [recordedChunks, setRecordedChunks] = useState<Blob[]>([])
-	const [mediaUrl, setMediaUrl] = useState<string>("")
-	const recordingFinish = useRef<boolean>(false)
-	const [recordingPaused, setRecordingPaused] = useState<boolean>(false)
+	);
+	// const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
+	const [mediaUrl, setMediaUrl] = useState<string>("");
+	const recordingFinish = useRef<boolean>(false);
+	const [recordingPaused, setRecordingPaused] = useState<boolean>(false);
 
 	const openMediaDevices = async (constraints: any) => {
 		try {
-			// Check if getDisplayMedia is supported
 			if (!navigator.mediaDevices.getDisplayMedia) {
-				console.error("getDisplayMedia is not supported on this device")
-				toast.error(
-					"Sorry, your browser does not support screen recording."
-				)
-				return null
+				console.error("getDisplayMedia is not supported on this device");
+				toast.error("Sorry, your browser does not support screen recording.");
+				return null;
 			}
 
 			const screenStream = await navigator.mediaDevices.getDisplayMedia(
 				constraints
-			)
-			return screenStream
+			);
+			return screenStream;
 		} catch (error: any) {
-			console.error("Error opening media devices", error)
+			console.error("Error opening media devices", error);
 			if (error.name === "NotAllowedError") {
-				toast.error("Permission denied to use media devices")
+				toast.error("Permission denied to use media devices");
 			} else {
-				toast.error("Unable to use media devices")
+				toast.error("Unable to use media devices");
 			}
-			return null
+			return null;
 		}
-	}
+	};
 
 	const openUserMedia = async (constraints: any) => {
 		try {
-			const userStream = await navigator.mediaDevices.getUserMedia(
-				constraints
-			)
-			return userStream
+			const userStream = await navigator.mediaDevices.getUserMedia(constraints);
+			return userStream;
 		} catch (error: any) {
-			console.error("Error opening user media", error)
+			console.error("Error opening user media", error);
 			if (error.name === "NotAllowedError") {
-				toast.error("Permission denied to use media devices")
+				toast.error("Permission denied to use media devices");
 			} else {
-				toast.error("Unable to use media devices")
+				toast.error("Unable to use media devices");
 			}
-			return null
+			return null;
 		}
-	}
+	};
 
 	const startRecording = async () => {
 		if (selectedOption === "") {
-			toast.error("Choose your recording preference")
-			return
+			toast.error("Choose your recording preference");
+			return;
 		}
 
-		let constraints: any = { video: true }
+		let constraints: any = { video: true };
 
 		if (selectedOption === "screenOnly") {
-			constraints = { video: { mediaSource: "screen" } }
+			constraints = { video: { mediaSource: "screen" } };
 		} else if (
 			selectedOption === "screenWithAudio" ||
 			selectedOption === "screenWithAudioAndMic"
@@ -100,97 +121,82 @@ export const MediaProvider: React.FC<{ children: ReactNode }> = ({
 					noiseSuppression: true,
 					sampleRate: 44100,
 				},
-			}
+			};
 		}
 
-		const screenStream = await openMediaDevices(constraints)
-
-		if (screenStream == null) return
+		const screenStream = await openMediaDevices(constraints);
+		if (!screenStream) return;
 
 		if (selectedOption === "screenWithAudioAndMic") {
-			const userStream = await openUserMedia({ audio: true })
-
-			if (userStream != null) {
-				if (screenStream == null) return
-
-				// Merge screen and mic audio tracks
-				const micAudioTracks = userStream.getAudioTracks()[0]
-				const screenAudioTrack = screenStream?.getAudioTracks()[0]
-
-				const stream = new MediaStream()
-
-				// Add mic audio track
-				stream.addTrack(micAudioTracks)
-
-				// Add screen audio track
-				if (screenAudioTrack) stream.addTrack(screenAudioTrack)
-
-				screenStream.getVideoTracks().forEach((videoTrack) => {
-					stream.addTrack(videoTrack)
-				})
-
-				// Set the new stream which contains both screen and mic audio
-				setStream(stream)
+			const micStream = await openUserMedia({ audio: true });
+			if (micStream) {
+				const combinedStream = mergeAudioStreams(screenStream, micStream);
+				setStream(combinedStream);
+			} else {
+				setStream(screenStream);
 			}
+		} else {
+			setStream(screenStream);
 		}
 
-		if (stream == null) {
-			// Set the screen stream if no mic audio is needed
-			setStream(screenStream)
-		}
-
-		setRecording(true)
-		setMediaUrl("")
-		recordingFinish.current = false
-	}
+		setRecording(true);
+		setMediaUrl("");
+		recordingFinish.current = false;
+	};
 
 	const pauseRecording = () => {
-		if (mediaRecorder == null) return
-		mediaRecorder.pause()
-		setRecordingPaused(true)
-	}
+		if (mediaRecorder == null) return;
+		mediaRecorder.pause();
+		setRecordingPaused(true);
+	};
 
 	const resumeRecording = () => {
-		if (mediaRecorder == null) return
-		mediaRecorder.resume()
-		setRecordingPaused(false)
-	}
+		if (mediaRecorder == null) return;
+		mediaRecorder.resume();
+		setRecordingPaused(false);
+	};
 
 	const stopRecording = useCallback(() => {
-		if (mediaRecorder == null || recordingFinish.current) return
+		if (mediaRecorder == null || recordingFinish.current) return;
 
-		// Prevent to call stop multiple times
-		const isInactive = mediaRecorder.state === "inactive"
-		if (!isInactive) mediaRecorder.stop()
+		if (mediaRecorder.state !== "inactive") mediaRecorder.stop();
 
-		const blob = new Blob(recordedChunks)
-		const url = URL.createObjectURL(blob)
-		setMediaUrl(url)
-		setRecordedChunks([])
-		setRecording(false)
-		recordingFinish.current = true
-		stream?.getTracks().forEach((track) => track.stop())
-	}, [mediaRecorder, recordedChunks, stream])
+		recordingFinish.current = true;
+		setRecording(false);
+		stream?.getTracks().forEach((track) => track.stop());
+	}, [mediaRecorder, stream]);
 
 	useEffect(() => {
-		if (stream == null) return
+		if (!stream) return;
 
-		if (mediaRecorder == null) {
-			const recorder: MediaRecorder = new MediaRecorder(stream)
-			recorder.start(15)
-			setMediaRecorder(recorder)
-		} else {
-			mediaRecorder.ondataavailable = (e) => {
-				if (e.data.size > 0) {
-					setRecordedChunks((prevChunks) => [...prevChunks, e.data])
-				}
+		const mimeType = "video/webm; codecs=vp8,opus";
+		const options = MediaRecorder.isTypeSupported(mimeType) ? { mimeType } : {};
+
+		const recorder = new MediaRecorder(stream, options);
+		setMediaRecorder(recorder);
+
+		const chunks: Blob[] = [];
+
+		recorder.ondataavailable = (e) => {
+			if (e.data.size > 0) {
+				chunks.push(e.data);
 			}
-			mediaRecorder.onstop = stopRecording
-			{
-				;(mediaRecorder.stream as any).oninactive = stopRecording
-			}
-		}
-	}, [mediaRecorder, recordedChunks, stopRecording, stream])
+		};
+
+		recorder.onstop = () => {
+			const blob = new Blob(chunks, { type: chunks[0]?.type || "video/webm" });
+			const url = URL.createObjectURL(blob);
+			setMediaUrl(url);
+			// setRecordedChunks([]); // Clear old chunks
+			recordingFinish.current = true;
+		};
+
+		recorder.start(15);
+
+		return () => {
+			if (recorder.state !== "inactive") recorder.stop();
+		};
+	}, [stream]);
 
 	return (
 		<MediaContext.Provider
@@ -209,5 +215,5 @@ export const MediaProvider: React.FC<{ children: ReactNode }> = ({
 		>
 			{children}
 		</MediaContext.Provider>
-	)
-}
+	);
+};
